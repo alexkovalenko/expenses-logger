a7f7a4177740f08086b8b921da205a59ff213d2e
alexkovalenko <alex.kov.job@gmail.com>
2016-10-22 23:21:51 +0300

    expense form server side

--- server/src/main/java/org/alexkovalenko/service/ExpenseService.java
+++ server/src/main/java/org/alexkovalenko/service/ExpenseService.java
@@ -1,21 +1,26 @@
 package org.alexkovalenko.service;
 
-import org.alexkovalenko.data.generated.tables.daos.ExpenseDao;
 import org.alexkovalenko.data.generated.tables.pojos.Expense;
+import org.alexkovalenko.data.generated.tables.records.ExpenseRecord;
+import org.jooq.DSLContext;
+import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class ExpenseService {
 
-    private final ExpenseDao dao;
+    private final DSLContext context;
+    private final ModelMapper mapper;
 
     @Autowired
-    public ExpenseService(ExpenseDao dao) {
-        this.dao = dao;
+    public ExpenseService(DSLContext context, ModelMapper mapper) {
+        this.context = context;
+        this.mapper = mapper;
     }
 
     public void addExpense(Expense expense) {
-        dao.insert(expense);
+        ExpenseRecord record = mapper.map(expense, ExpenseRecord.class);
+        context.batchStore(record).execute();
     }
 }
